7b5f8da06ae965613f1681df4dd38472b30d4cf0
Yulia Monti <yuliamonti@gmail.com>
2022-10-11 21:59:55 -0300

    Clase 3, Iniciando componentes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <>
+        <h2>Soy el footer</h2>
+        </>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+export const Main = () => {
+    return (
+        <>
+        <h2>Soy el Main</h2>
+        </>
+    )
+}
